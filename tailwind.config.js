51380c6e54c4132e93a7bb3cbd570c263c1eb4f2
guidagg <guida.gribeiro@gmail.com>
2023-10-27 16:18:36 +0200

    feat: add correct font and font sizes to theme
    - correct logo and header sizes
    - apply rules to header and netzerk page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
@@ -10,7 +11,18 @@ export default {
         'oliv-lt': '#E3E4D7',
         'oliv-dk': '#A2C748',
       },
+      fontFamily: {
+				medium: ['CabinetGrotesk-Medium', 'sans-serif'],
+				bold: ['CabinetGrotesk-Bold', 'sans-serif']
+			},
     },
+    fontSize: {
+			'2xl': ['var(--nzn-text-2xl)', 'var(--nzn-line-2xl)'],
+			xl: ['var(--nzn-text-xl)', 'var(--nzn-line-xl)'],
+			base: ['var(--nzn-text-base)', 'var(--nzn-line-base)'],
+			sm: ['var(--nzn-text-sm)', 'var(--nzn-line-sm)'],
+			xs: ['var(--nzn-text-xs)', 'var(--nzn-line-sm)']
+		},
   },
   plugins: [],
 }
