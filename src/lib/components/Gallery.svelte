32f789a62a729c16dcba06d83ec17db43263a9d7
guidagg <guida.gribeiro@gmail.com>
2023-12-11 15:22:28 +0100

    feat: basic news detail view
    - gallery not finished

---
+++ src/lib/components/Gallery.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+import type { Image } from '$lib/types';
+import ImageComponent from './Image.svelte';
+
+export let gallery: Image[]
+
+let currentImage = 0;
+
+
+</script>
+
+<div class="relative overflow-hidden">
+    <div class="flex flex-col md:flex-row gap-5 relative flex-wrap">
+        {#each gallery as image}
+            <ImageComponent {image} size="large" class="md:max-w-[30%] min-w-0 w-auto"/>
+        {/each}
+    </div>
+</div>
--- src/routes/news/[slug]/+page.ts
+++ src/routes/news/[slug]/+page.ts
@@ -1,6 +1,61 @@
-// export const prerender = true;
-export const load = ({ params }) => {
-    return {
-        slug: params.slug
+import type { PageLoad } from './$types';
+import { gql } from 'graphql-request';
+import { client } from '$lib/api';
+import { flattenJson } from '$lib/utils';
+
+const newsQuery = gql`
+{
+    posts {
+      data {
+        id
+        attributes { 
+          title
+          slug
+          content {
+            title
+            body
+          }
+          publishedAt
+          gallery {
+                data {
+                    id
+                    attributes{
+                        name
+                        alternativeText
+                        url
+                        formats
+                    }
+                }
+            }
+            file {
+                        data {
+                            id
+                            attributes{
+                                name
+                                alternativeText
+                                url
+                                formats
+                            }
+                        }
+            }
+            title_file     
+        }      
     }
+  }
 }
+`
+export const load: import('./$types').PageLoad = (async () => {
+
+    try {
+        const dataNews = await client.request(newsQuery);
+        return {
+          news: flattenJson(dataNews.posts)
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+
+        return {
+          data: []
+        };
+      }
+}) 
