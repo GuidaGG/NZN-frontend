fced96cf732665ddd5bebe10b32b43bd20bc22c0
guidagg <guida.gribeiro@gmail.com>
2023-12-15 11:22:27 +0100

    fix: remove old svelte-pdf package and add new search url

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -5,6 +5,6 @@ import { MeiliSearch } from 'meilisearch'
 export const client = new GraphQLClient(`${config.apiUrl}/graphql`);
 
 export const searchClient = new MeiliSearch({
-    host: 'http://49.13.77.99:7700',
+    host: 'https://search.netzwerkzwischennutzung.de',
     apiKey: 'tdg9hmRBUg7OrlRj63Z5XSJH4YrX7dizjNPF-3Q72qM',
   })
